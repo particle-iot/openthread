ea382ea6002ce41af4151bac64e26b36a1e3cb49
Abtin Keshavarzian <abtink@google.com>
2019-02-13 18:17:18 -0800

    [slaac] add config option to enable/disable SLAAC support (#3512)
    This commit adds `OPENTHREAD_CONFIG_ENABLE_SLAAC` as a build
    configuration option to enable/disable SLAAC support within
    OpenThread. This feature is disabled by default. This commit also
    adds `SLAAC` to `common-switches` list to allow builds to enable
    this feature easily.

--- src/core/thread/mle.cpp
+++ src/core/thread/mle.cpp
@@ -1514,7 +1514,9 @@ void Mle::HandleStateChanged(otChangedFlags aFlags)
 #endif
 #endif
 
+#if OPENTHREAD_CONFIG_ENABLE_SLAAC
         GetNetif().UpdateSlaac();
+#endif
 
 #if OPENTHREAD_ENABLE_DHCP6_SERVER
         GetNetif().GetDhcp6Server().UpdateService();
